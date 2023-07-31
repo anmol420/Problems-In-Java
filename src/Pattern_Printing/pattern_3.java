eeccd4d732b44fe6d8a86bcef57b945cb61e6881
anmol420 <anmolanand2k15@gmail.com>
2023-08-01 01:06:42 +0530

    Added Pattern

--- src/Pattern_Printing/pattern_3.java
+++ src/Pattern_Printing/pattern_3.java
@@ -1,14 +1,15 @@
 /*
-*
-* *
-* * *
 * * * *
+* * *
+* *
+*
 */
+
 package Pattern_Printing;
 
-public class pattern_2 {
+public class pattern_3 {
     public static void main(String[] args) {
-        for (int i=1;i<=4;i++) {
+        for (int i=4;i>=1;i--) {
             for (int j=1;j<=i;j++) {
                 System.out.print("*");
             }
