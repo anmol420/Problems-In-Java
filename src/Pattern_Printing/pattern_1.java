75d01e40abbb9a2192b8b462e0b472060132917d
anmol420 <anmolanand2k15@gmail.com>
2023-07-31 14:36:31 +0530

    Initial Commit

---
+++ src/Pattern_Printing/pattern_1.java
@@ -0,0 +1,16 @@
+package Pattern_Printing;
+
+public class pattern_1 {
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++) {
+            for (int j=0;j<4;j++) {
+                if (i==0 || j==0 || i==4 || j==3) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
