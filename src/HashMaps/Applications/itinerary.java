b9c2d140bcc4cd384409053d90b37bb05077be16
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-08-29 23:57:18 +0530

    Added Application Codes

---
+++ src/HashMaps/Applications/itinerary.java
@@ -0,0 +1,39 @@
+package Applications;
+
+import java.util.*;
+
+public class itinerary {
+    public static String getStart(HashMap<String, String> tick) {
+        HashMap<String, String> revMap=new HashMap<>();
+        
+        for(String key: tick.keySet()) {
+            revMap.put(tick.get(key), key);
+        }
+
+        for (String key: tick.keySet()) {
+            if(!revMap.containsKey(key)) {
+                return key;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, String> map=new HashMap<>();
+
+        map.put("Chennai", "Bengaluru");
+        map.put("Mumbai", "Delhi");
+        map.put("Goa", "Chennai");
+        map.put("Delhi", "Goa");
+
+        String start=getStart(map);
+
+        while(map.containsKey(start)) {
+            System.out.print(start+"->");
+            start=map.get(start);
+        }
+
+        System.out.println(start);
+    }
+}
