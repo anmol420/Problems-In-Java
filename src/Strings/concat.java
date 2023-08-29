5c3cad07f309d2d9d3a3438c824dc555369af8f7
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-08-29 11:17:17 +0000

    Added Strings

---
+++ src/Strings/concat.java
@@ -0,0 +1,10 @@
+package Strings;
+
+class concat {
+    public static void main(String[] args) {
+        String n1="anmol";
+        String n2="420";
+        String name=n1.concat(n2);
+        System.out.println(name);
+    }
+}
---
+++ src/Strings/other_methods.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class other_methods {
+    public static void main(String[] args) {
+        String n1="anmol420";
+
+        System.out.println(n1.length());
+        
+        for (int i=0;i<n1.length();i++) {
+            char ch=n1.charAt(i);
+            System.out.println(ch);
+        }
+    }
+}
