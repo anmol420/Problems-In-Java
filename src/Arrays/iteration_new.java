3108463f1e8108aa3492a96b7c67a73899b8f5d9
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-08-29 17:11:40 +0530

    Added File

---
+++ src/Arrays/iteration_new.java
@@ -0,0 +1,9 @@
+public class iteration_new {
+    public static void main(String[] args) {
+        int a[]={12,8,14};
+
+        for (int i:a) {
+            System.out.println(i);
+        }
+    }
+}
